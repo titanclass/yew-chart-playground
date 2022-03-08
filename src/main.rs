a8d050d69878a4b3734ca10be44e3693fd8712f2
huntc <huntchr@gmail.com>
2022-03-08 12:22:29 +1100

    Improved labels

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,8 @@ fn app() -> Html {
         Box::new(move |_| "".to_string())
     }
 
-    let h_scale = Rc::new(LinearAxisScale::with_labeller(
+    let h_scale_top = Rc::new(LinearAxisScale::new(0.0..5.0, 1.0)) as Rc<dyn AxisScale>;
+    let h_scale_bottom = Rc::new(LinearAxisScale::with_labeller(
         0.0..5.0,
         1.0,
         Rc::new(empty_labeller()),
@@ -28,44 +29,36 @@ fn app() -> Html {
 
     html! {
         <>
-            <svg class="chart" viewBox={format!("0 0 {} {}", WIDTH, HEIGHT)} preserveAspectRatio="none">
-                <VerticalAxis
-                    name="some-y-axis"
-                    orientation={vertical_axis::Orientation::Left}
-                    scale={Rc::clone(&v_scale)}
-                    x1={MARGIN} y1={MARGIN} y2={HEIGHT - TICK_LENGTH}
-                    tick_len={TICK_LENGTH}
-                    title={"Some Y thing".to_string()} />
-                <HorizontalAxis
-                    name="some-x-axis"
-                    orientation={horizontal_axis::Orientation::Bottom}
-                    scale={Rc::clone(&h_scale)}
-                    x1={MARGIN} y1={HEIGHT - TICK_LENGTH} x2={WIDTH}
-                    tick_len={TICK_LENGTH} />
-            </svg>
-            <svg class="chart" viewBox={format!("0 0 {} {}", WIDTH, TICK_LENGTH)} preserveAspectRatio="none">
-                <HorizontalAxis
-                    name="some-x-axis"
-                    orientation={horizontal_axis::Orientation::Bottom}
-                    scale={Rc::clone(&h_scale)}
-                    x1={MARGIN} y1={0.0} x2={WIDTH}
-                    tick_len={TICK_LENGTH} />
-            </svg>
-            <svg class="chart" viewBox={format!("0 0 {} {}", WIDTH, HEIGHT)} preserveAspectRatio="none">
-                <HorizontalAxis
-                    name="some-x-axis"
-                    orientation={horizontal_axis::Orientation::Top}
-                    scale={Rc::clone(&h_scale)}
-                    x1={MARGIN} y1={TICK_LENGTH} x2={WIDTH}
-                    tick_len={TICK_LENGTH} />
-                <VerticalAxis
-                    name="some-y-axis"
-                    orientation={vertical_axis::Orientation::Left}
-                    scale={Rc::clone(&v_scale)}
-                    x1={MARGIN} y1={TICK_LENGTH} y2={HEIGHT - MARGIN}
-                    tick_len={TICK_LENGTH}
-                    title={"Some Y thing".to_string()} />
-            </svg>
+        <svg class="chart" viewBox={format!("0 0 {} {}", WIDTH, HEIGHT)} preserveAspectRatio="none">
+            <VerticalAxis
+                name="some-y-axis"
+                orientation={vertical_axis::Orientation::Left}
+                scale={Rc::clone(&v_scale)}
+                x1={MARGIN} y1={MARGIN} y2={HEIGHT - TICK_LENGTH * 2.0}
+                tick_len={TICK_LENGTH}
+                title={"Some Y thing".to_string()} />
+            <HorizontalAxis
+                name="some-x-axis"
+                orientation={horizontal_axis::Orientation::Bottom}
+                scale={Rc::clone(&h_scale_top)}
+                x1={MARGIN} y1={HEIGHT - TICK_LENGTH * 2.0} x2={WIDTH - MARGIN}
+                tick_len={TICK_LENGTH} />
+        </svg>
+        <svg class="chart" viewBox={format!("0 0 {} {}", WIDTH, HEIGHT)} preserveAspectRatio="none">
+            <HorizontalAxis
+                name="some-x-axis"
+                orientation={horizontal_axis::Orientation::Top}
+                scale={Rc::clone(&h_scale_bottom)}
+                x1={MARGIN} y1={TICK_LENGTH} x2={WIDTH - MARGIN}
+                tick_len={TICK_LENGTH} />
+            <VerticalAxis
+                name="some-y-axis"
+                orientation={vertical_axis::Orientation::Left}
+                scale={Rc::clone(&v_scale)}
+                x1={MARGIN} y1={TICK_LENGTH} y2={HEIGHT - MARGIN + TICK_LENGTH}
+                tick_len={TICK_LENGTH}
+                title={"Some Y thing".to_string()} />
+        </svg>
         </>
     }
 }
